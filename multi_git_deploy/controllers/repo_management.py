e1dbdbb28597280c7ad137e7bbabcc0ba198b46d
EdgeJ <edge.jm@gmail.com>
2017-10-23 22:49:25 -0400

    adding diff function and using session to persist connections

--- multi_git_deploy/controllers/repo_management.py
+++ multi_git_deploy/controllers/repo_management.py
@@ -12,14 +12,16 @@
 import requests
 from multi_git_deploy import app
 
+API_SESSION = requests.Session()
+API_SESSION.headers.update(app.config['GITLAB_TOKEN_HEADER'])
+
 
 def list_repos():
     """
     Return json object of all repos.
     """
-    repos = requests.get(
+    repos = API_SESSION.get(
         '{}/projects'.format(app.config['GITLAB_URL']),
-        headers=app.config['GITLAB_TOKEN_HEADER']
     )
     return repos.json()
 
@@ -28,12 +30,11 @@ def get_repo(repo_id):
     """
     Return json object of a single repo.
     """
-    repo = requests.get(
+    repo = API_SESSION.get(
         '{url}/projects/{repo_id}'.format(
             url=app.config['GITLAB_URL'],
             repo_id=repo_id
-        ),
-        headers=app.config['GITLAB_TOKEN_HEADER']
+        )
     )
     return repo.json()
 
@@ -42,12 +43,11 @@ def get_branches(repo_id):
     """
     Return json object of all branches of a repo.
     """
-    branches = requests.get(
+    branches = API_SESSION.get(
         '{url}/projects/{id}/repository/branches'.format(
             url=app.config['GITLAB_URL'],
             id=repo_id
-        ),
-        headers=app.config['GITLAB_TOKEN_HEADER']
+        )
     )
     return branches.json()
 
@@ -56,12 +56,49 @@ def get_commits(repo_id, branch):
     """
     Return json object of all commits for a branch of a repo.
     """
-    commits = requests.get(
+    commits = API_SESSION.get(
         '{url}/projects/{id}/repository/commits?ref_id={branch}'.format(
             url=app.config['GITLAB_URL'],
             id=repo_id,
             branch=branch
-        ),
-        headers=app.config['GITLAB_TOKEN_HEADER']
+        )
     )
     return commits.json()
+
+
+def show_diff(repo_id, commit_hash):
+    """TODO: Docstring for show_diff.
+
+    :repo_id: id of the repo.
+    :commit_hash: SHA hash of the commit to show a diff for.
+    :returns: TODO
+
+    """
+    diff = API_SESSION.get(
+        '{url}/projects/{id}/repository/commits/{sha}/diff'.format(
+            url=app.config['GITLAB_URL'],
+            id=repo_id,
+            sha=commit_hash
+        )
+    )
+    return diff.json()
+
+
+def cherry_pick(repo_id, target_branch, commit_hash):
+    """
+    Cherry pick a commit into another branch via the Gitlab API.
+
+    :repo_id: id of the repo to commit to.
+    :target_branch: target branch to commit to.
+    :commit_hash: SHA hash for the commit being cherry picked.
+    :returns: JSON object from the Gitlab API request.
+
+    """
+    commit_string = API_SESSION.post(
+        '{url}/projects/{id}/repository/commits/{hash}/cherry_pick'.format(
+            url=app.config['GITLAB_URL'],
+            id=repo_id,
+            hash=commit_hash
+        ), data={'branch': target_branch}
+    )
+    return commit_string.json()
